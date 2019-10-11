f29325a05b35bc4dab3aa83140b63148064608e5
captainfrak <saez.sylvain@gmail.com>
2019-10-11 13:20:46 +0200

    Clean with PHPCS

--- Controllers/PageController.php
+++ Controllers/PageController.php
@@ -1,9 +1,35 @@
 <?php
+/**
+ * Controller for common pages of the blog
+ *
+ * PHP Version 7.+
+ *
+ * @category  Controllers
+ * @package   Controllers
+ * @author    Sylvain SAEZ <saez.sylvain@gmail.com>
+ * @copyright 2019 Frakdev
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link      sylvainsaez.fr
+ */
 
 namespace Controllers;
 
+/**
+ * Class PageController
+ *
+ * @category Controllers
+ * @package  Controllers
+ * @author   Sylvain SAEZ <saez.sylvain@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     sylvainsaez.fr
+ */
 class PageController extends Controller
 {
+    /**
+     * Render the home page and manage the contact form
+     *
+     * @return void
+     */
     public function homePage()
     {
         //if there is, get the inputs
@@ -15,7 +41,8 @@ class PageController extends Controller
             //Mail function
             $to = 'cpt.frak@me.com';
             $email_subject = "Nouveau message de la part de $name";
-            $email_body = "Formulaire de contact du site,<br> $message <br> Répondre à $email";
+            $email_body = "Formulaire de contact du site,
+                <br> $message <br> Répondre à $email";
             $headers = "From: noreply@sylvainsaez.fr";
             $headers .= "Reply-To: $email";
             mail($to, $email_subject, $email_body, $headers);
@@ -25,11 +52,21 @@ class PageController extends Controller
         $this->render('index.html.twig');
     }
 
+    /**
+     * Render the 404 page for the site
+     *
+     * @return void
+     */
     public function errorPage()
     {
         $this->render('404.html.twig');
     }
 
+    /**
+     * Destroy the session and redirect to the home page
+     *
+     * @return void
+     */
     public function logout()
     {
         session_destroy();
