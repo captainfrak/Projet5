9406f1abb723342053c96d8948c3c887f8d9e885
captainfrak <saez.sylvain@gmail.com>
2019-10-11 17:57:38 +0200

    Add the doc comment to the file

--- cli-config.php
+++ cli-config.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Config file of the blog
+ *
+ * PHP Version 7.+
+ *
+ * @category  Projet5
+ * @package   Projet5
+ * @author    Sylvain SAEZ <saez.sylvain@gmail.com>
+ * @copyright 2019 Frakdev
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link      sylvainsaez.fr
+ */
 
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 use System\Database;
