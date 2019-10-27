5fe4678d7f9d2aa73726a507416565d7cb5ce40e
captainfrak <saez.sylvain@gmail.com>
2019-10-27 15:30:55 +0100

    Refacto : corrected a string concat to a single string instead

--- System/autoload.php
+++ System/autoload.php
@@ -19,6 +19,6 @@ spl_autoload_register(
  */
     function ($class) {
         $class = str_replace("\\", "/", $class);
-        include_once __DIR__ . '/' . "../" . $class . ".php";
+        include_once __DIR__ . "/../" . $class . ".php";
     }
 );
